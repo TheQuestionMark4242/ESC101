6fec4a754c85ed04ca1bba25f8caaa6b9dc2a51e
TheQuestionMark4242 <73532221+TheQuestionMark4242@users.noreply.github.com>
2020-12-10 02:13:50 +0530

    Create ReadTillEscapeCharacter.c

---
+++ Tools/ReadTillEscapeCharacter.c
@@ -0,0 +1,6 @@
+ int run; //Change type here
+    scanf("%d", &run);   //Change Modifier Here
+    while(run != -1){    //Change Escape Character here
+        /*Insert Processing Here*/
+        scanf("%d", &run);
+    }
