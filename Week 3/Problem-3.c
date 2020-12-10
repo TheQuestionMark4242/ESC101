e116a2390bad52cb35f289f611e32c0e22006534
TheQuestionMark4242 <73532221+TheQuestionMark4242@users.noreply.github.com>
2020-12-10 22:10:49 +0530

    Create Problem-3.c

---
+++ Week 3/Problem-3.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main(){
+    int N, M, temp, flag=0, jump=0,score=0;
+    int x=0,y=0; //Store the position 
+    scanf("%d %d", &N, &M);
+    
+    for(int i = 0; i < N ; i++){
+        for(int j = 0; j < M; j++){
+            scanf("%d", &temp);
+            if((i < y)||( j < x )){ //Skip to next position when we have jumped
+                continue;
+            }
+            if (jump == 1){ //Check if moving to next row is possible
+                if (((x == j)||(x == j-1))&&(temp ==0)){
+                    jump=0;
+                    continue;
+                }
+                else if (((j==(x+1)||(j==M-1))&&(temp!=0))){ //If not 
+                    flag=1;
+                    continue;
+                }
+                else{
+                    continue;
+                }
+            }
+            if (temp==0){
+                x = j;
+                y = i;
+            }
+            else {
+                y++; //Move down when we can't move along row
+            }
+        }
+        score+=(x*x*x);
+        jump = 1;
+    }
+    if(flag==1){
+        printf("WRONG MAZE");
+    }
+    else{
+        printf("%d",score);
+    }
+    return 0;
+}
