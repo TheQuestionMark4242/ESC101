f92eb1b402e3388ea600c53c84a74ae913f692eb
TheQuestionMark4242 <73532221+TheQuestionMark4242@users.noreply.github.com>
2021-01-21 15:44:24 +0530

    Create 1.c

---
+++ Week-7/1.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAXLINE 100
+int isBetween(char c){
+    if (((c>='d')&&(c<='s'))||((c>='D')&&(c<='S'))){
+        return 1;
+    }
+    else return 0;
+}
+
+void swap(char *a, char *b){
+    char temp;
+    temp =*a;
+    *a = *b;
+    *b = temp;
+}
+
+void read(char *c, int *i){
+    char t = getchar();
+    *i = 0;
+    while(t!=EOF){
+        *(c+(*i)) = t;
+        (*i)++;
+        t = getchar();
+    }
+}
+void rev(char *c, int N ){
+    if (N>1){
+        if (isBetween(*(c))&&isBetween(*(c+N-1))){
+            swap(c, c+N-1);
+        }
+        rev(c+1,N-2);
+    }
+}
+int main(){
+    char c[MAXLINE];
+    int N;
+    read(c,&N);
+    rev(c,N);
+    for(int j = 0; j<N;j++){
+        printf("%c",c[j]);
+    }
+    return 0;
+}
